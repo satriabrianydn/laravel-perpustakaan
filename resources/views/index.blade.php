4457f86bffcfe7b380b5123566682039dd67df6b
satriabrianydn <satriabrianyudanto16@gmail.com>
2024-01-07 17:12:17 +0700

    nothing to update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,8 @@
                     <div class="carousel-caption d-md-block">
                         <h5 class="animate__animated animate__fadeInDown">Universitas Duta Bangsa Surakarta</h5>
                         <div class="animate__animated animate__fadeInUp">
-                            <p>Terima kasih telah memilih Perpustakaan Universitas Duta Bangsa sebagai mitra pendidikan dan penelitian Anda.</p>
+                            <p>Terima kasih telah memilih Perpustakaan Universitas Duta Bangsa sebagai mitra pendidikan dan
+                                penelitian Anda.</p>
                         </div>
                         <div class="carousel-button animate__animated animate__fadeInLeft">
                             <a class="btn btn-warning" href="#">Read More</a>
@@ -26,7 +27,8 @@
                     <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="Slide 2">
                     <div class="carousel-caption d-md-block">
                         <h5 class="animate__animated animate__fadeInDown">Pelayanan Kami</h5>
-                        <p class="animate__animated animate__fadeInUp">Kami berkomitmen untuk terus meningkatkan layanan kami demi mendukung kesuksesan akademis Anda.</p>
+                        <p class="animate__animated animate__fadeInUp">Kami berkomitmen untuk terus meningkatkan layanan
+                            kami demi mendukung kesuksesan akademis Anda.</p>
                         <div class="carousel-button animate__animated animate__fadeInLeft">
                             <a class="btn btn-warning" href="#">Read More</a>
                         </div>
@@ -36,7 +38,8 @@
                     <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="Slide 3">
                     <div class="carousel-caption d-md-block">
                         <h5 class="animate__animated animate__fadeInDown">Motivasi Belajar</h5>
-                        <p class="animate__animated animate__fadeInUp">Mari berlayar melalui kata-kata dan biarkan setiap halaman membawa kita lebih dekat pada versi terbaik dari diri kita.</p>
+                        <p class="animate__animated animate__fadeInUp">Mari berlayar melalui kata-kata dan biarkan setiap
+                            halaman membawa kita lebih dekat pada versi terbaik dari diri kita.</p>
                         <div class="carousel-button animate__animated animate__fadeInLeft">
                             <a class="btn btn-warning" href="#">Read More</a>
                         </div>
@@ -59,7 +62,10 @@
 
     <div class="container perkenalan">
         <h2>SELAMAT DATANG</h2>
-        <p>Sebuah pusat pengetahuan yang berkomitmen untuk memberikan layanan terbaik dalam mendukung pembelajaran, penelitian, dan pengembangan masyarakat. Sebagai bagian integral dari Universitas Duta Bangsa, perpustakaan kami menjadi pusat informasi yang dinamis, memberikan akses luas terhadap berbagai sumber daya akademis untuk memperkaya pengalaman belajar setiap mahasiswa dan peneliti.</p>
+        <p>Sebuah pusat pengetahuan yang berkomitmen untuk memberikan layanan terbaik dalam mendukung pembelajaran,
+            penelitian, dan pengembangan masyarakat. Sebagai bagian integral dari Universitas Duta Bangsa, perpustakaan kami
+            menjadi pusat informasi yang dinamis, memberikan akses luas terhadap berbagai sumber daya akademis untuk
+            memperkaya pengalaman belajar setiap mahasiswa dan peneliti.</p>
     </div>
 
     <div class="container">
@@ -68,74 +74,77 @@
                 <div class="flex-row">
 
                     <!-- Artikel Pertama -->
-<div class="berita-box">
-    <article>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <img src="Bagus_Adi.png" alt="Kepala Perpustakaan Photo" class="member-photo" width="300" height="280">
-            </div>
-            <div class="col-sm-8">
-                <h6 class="category text-info">
-                    <a href="#" rel="tag">
-                        Kepala Perpustakaan
-                    </a>
-                </h6>
-                <h2><a href="#">BAGUS ADI</a></h2>
-                <p class="meta" style="margin-bottom: 100px;">
-                    <span class="author">NO.Telp: 082754265674</span>
-                </p>
-            </div>
-        </div>
-    </article>
-    <!-- Anggota Pertama End -->
+                    <div class="berita-box">
+                        <article>
+                            <div class="row">
+                                <div class="col-sm-4 text-center">
+                                    <img src="Bagus_Adi.png" alt="Kepala Perpustakaan Photo" class="member-photo"
+                                        width="300" height="280">
+                                </div>
+                                <div class="col-sm-8">
+                                    <h6 class="category text-info">
+                                        <a href="#" rel="tag">
+                                            Kepala Perpustakaan
+                                        </a>
+                                    </h6>
+                                    <h2><a href="#">BAGUS ADI</a></h2>
+                                    <p class="meta" style="margin-bottom: 100px;">
+                                        <span class="author">NO.Telp: 082754265674</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                        <!-- Anggota Pertama End -->
 
-    <!-- Anggota Kedua -->
-    <article>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <img src="Asep_Bastian.png" alt="Anggota Kedua Photo" class="member-photo" width="190" height="300">
-            </div>
-            <div class="col-sm-4">
-                <h6 class="category text-info">
-                    <a href="#" rel="tag">
-                        Penanggung Jawab
-                    </a>
-                </h6>
-                <h2><a href="#">ASEP BASTIAN</a></h2>
-                <p class="meta" style="margin-bottom: 10px;">
-                    <span class="author">NO.Telp: 085836427846</span>
-                </p>
-            </div>
-        </div>
-    </article>
-    <!-- Anggota Kedua End -->
+                        <!-- Anggota Kedua -->
+                        <article>
+                            <div class="row">
+                                <div class="col-sm-4 text-center">
+                                    <img src="Asep_Bastian.png" alt="Anggota Kedua Photo" class="member-photo"
+                                        width="190" height="300">
+                                </div>
+                                <div class="col-sm-4">
+                                    <h6 class="category text-info">
+                                        <a href="#" rel="tag">
+                                            Penanggung Jawab
+                                        </a>
+                                    </h6>
+                                    <h2><a href="#">ASEP BASTIAN</a></h2>
+                                    <p class="meta" style="margin-bottom: 10px;">
+                                        <span class="author">NO.Telp: 085836427846</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                        <!-- Anggota Kedua End -->
 
-    <!-- Anggota Ketiga -->
-    <article>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <img src="Joko_Slamet.png" alt="Anggota Ketiga Photo" class="member-photo" width="220" height="280">
-            </div>
-            <div class="col-sm-4">
-                <h6 class="category text-info">
-                    <a href="#" rel="tag">
-                        Sekretaris
-                    </a>
-                </h6>
-                <h2><a href="#">JOKO SLAMET</a></h2>
-                <p class="meta" style="margin-bottom: 10px;">
-                    <span class="author">NO.Telp: 089675437556</span>
-                </p>
-            </div>
-        </div>
-    </article>
-    <!-- Anggota Ketiga End -->
-</div>
-
+                        <!-- Anggota Ketiga -->
+                        <article>
+                            <div class="row">
+                                <div class="col-sm-4 text-center">
+                                    <img src="Joko_Slamet.png" alt="Anggota Ketiga Photo" class="member-photo"
+                                        width="220" height="280">
+                                </div>
+                                <div class="col-sm-4">
+                                    <h6 class="category text-info">
+                                        <a href="#" rel="tag">
+                                            Sekretaris
+                                        </a>
+                                    </h6>
+                                    <h2><a href="#">JOKO SLAMET</a></h2>
+                                    <p class="meta" style="margin-bottom: 10px;">
+                                        <span class="author">NO.Telp: 089675437556</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                        <!-- Anggota Ketiga End -->
                     </div>
+
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
 
