46e9c8494a85abb7ece9358d0ac3554c6704bb66
jhnmira <amunifjihan03@gmail.com>
2024-01-11 07:34:09 +0700

    halaman view category 2

--- resources/views/category/agama_islam.blade.php
+++ resources/views/category/agama_islam.blade.php
@@ -1,13 +1,29 @@
 @extends('layouts.main')
 @section('title', 'Perpustakaan | Agama Islam')
 
-
 @section('main')
-
-<div class="container mt-3">
-    <div class="box-category">
-        <h4>AGAMA ISLAM</h4>
+    <div class="container mt-3">
+        <div class="box-category">
+            <h4 class="mb-4">Agama Islam</h4>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @forelse ($books as $book)
+                    <div class="col">
+                        <div class="card h-100 small-card">
+                            <img src="{{ asset('storage/' . ($book->foto_buku ?? 'covers/no_image_available.png')) }}"
+                                class="card-img-center" alt="Cover Buku">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $book->nama_buku }}</h5>
+                                <p class="card-text">{{ $book->nama_pengarang }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <p class="card-text">Deskripsi: {{ $book->deskripsi }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                <p>Belum ada buku dalam kategori ini.</p>
+                @endforelse
+            </div>
+        </div>
     </div>
-</div>
-    
 @endsection
