e62dd63439c0274ffdeb263215930239d6dd6350
jhnmira <amunifjihan03@gmail.com>
2024-01-11 07:16:37 +0700

    controller category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,55 +18,79 @@ class HomeController extends Controller
 
          // Ambil data buku dengan id_kategori yang sesuai
          $books = Book::where('id_kategori', $horrorCategoryId)->get();
- 
+
          return view('category.horror', ['books' => $books]);
     }
 
     public function showCategoryFIKSI() {
-        return view('category.fiksi');
+        $fiksiCategoryId = Kategori::where('kategori', 'fiksi')->value('id');
+        $books = Book::where('id_kategori', $fiksiCategoryId)->get();
+        return view('category.fiksi', ['books' => $books]);
     }
 
     public function showCategoryCERPEN() {
+        $cerpenCategoryId = Kategori::where('kategori', 'cerpen')->value('id');
+        $books = Book::where('id_kategori', $cerpenCategoryId)->get();
         return view('category.cerpen');
     }
 
     public function showCategoryNONFIKSI() {
+        $nonfiksiCategoryId = Kategori::where('kategori', 'nonfiksi')->value('id');
+        $books = Book::where('id_kategori', $nonfiksiCategoryId)->get();
         return view('category.nonfiksi');
     }
 
     public function showCategoryPUISI() {
+        $puisiKategoriId = Kategori::where('kategori', 'puisi')->value('id');
+        $books = Book::where('id_kategori', $puisiKategoriId)->get();
         return view('category.puisi');
     }
 
     public function showCategoryEkonomi() {
+        $ekonomikategori_id = Kategori::where('kategori', 'ekonomi')->value('id');
+        $books = Book::where('id_kategori', $ekonomikategori_id)->get();
         return view('category.ekonomi');
     }
 
     public function showCategoryMatematika() {
+        $matematikakateogori = Kategori::where('kategori', 'matematika')->value('id');
+        $books = Book::where('id_kategori', $matematikakateogori)->get();
         return view('category.matematika');
     }
 
     public function showCategoryFisika() {
+        $fisikacategory = Kategori::where('kategori', 'fisika')->value('id');
+        $books = Book::where('id_kategori', $fisikacategory)->get();
         return view('category.fisika');
     }
 
     public function showCategorySejarah() {
+        $sejarahkategori = kategori::where('kategori', 'sejarah')->value('id');
+        $books = Book::where('id_kategori', $sejarahkategori)->get();
         return view('category.sejarah');
     }
 
     public function showCategoryAgamaIslam() {
+        $agamakategori = kategori::where('kategori', 'agama')->value('id');
+        $books = Book::where('id_kategori', $agamakategori)->get();
         return view('category.agama_islam');
     }
 
     public function showCategoryBiologi() {
+        $biologikategori = kategori::where('kategori', 'biologi')->value('id');
+        $books = Book::where('id_kategori', $biologikategori)->get();
         return view('category.biologi');
     }
 
     public function showCategoryTeknologi() {
+        $teknologikategori = kategori::where('kategori', 'teknologi')->value('id');
+        $books = Book::where('id_kategori', $teknologikategori)->get();
         return view('category.teknologi');
     }
 
     public function showCategoryInggris() {
+        $inggriskategori = kategori::where('kategori', 'inggris')->value('id');
+        $books = Book::where('id_kategori', $inggriskategori)->get();
         return view('category.inggris');
     }
 }
