c2c453cfdcc17b3b9ced6abe8e872627b0c95459
satriabrianydn <satriabrianyudanto16@gmail.com>
2024-01-23 00:24:33 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         
-        $this->call([UserSeeder::class, KategoriSeeder::class]);
+        $this->call([UserSeeder::class]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
