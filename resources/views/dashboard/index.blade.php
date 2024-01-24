68443a5e9bb32cf11d1f5fd956d269e84715e950
satriabrianydn <satriabrianyudanto16@gmail.com>
2024-01-24 22:53:47 +0700

    update index dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,12 +5,12 @@
     <div class="row">
         @if (in_array(auth()->user()->role, ['Administrator', 'Petugas']))
             <div class="col-lg-12">
-                <div class="row">
+                <div class="row justify-content-center">
                     <div class="col-lg-3">
                         <div class="card"> <!-- Data User -->
                             <div class="card-body">
                                 <a href="{{ route('dashboard.user') }}" class="">
-                                    <div class="row alig n-items-start">
+                                    <div class="row align-items-start">
                                         <div class="col-8">
                                             <h5 class="card-title mb-9 fw-semibold">Total<br> Anggota</h5>
                                             <h4 class="fw-semibold mb-3">{{ $totalUser }}</h4>
@@ -67,29 +67,6 @@
                                                 <div
                                                     class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
                                                     <i class="ti ti-cards fs-6"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <a href="#" class="">
-                                    <div class="row alig n-items-start">
-                                        <div class="col-8">
-                                            <h5 class="card-title mb-9 fw-semibold">Total <br> Terlambat</h5>
-                                            <h4 class="fw-semibold mb-3">0</h4>
-
-                                        </div>
-                                        <div class="col-4">
-                                            <div class="d-flex justify-content-end">
-                                                <div
-                                                    class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                                                    <i class="ti ti-file-description fs-6"></i>
                                                 </div>
                                             </div>
                                         </div>
